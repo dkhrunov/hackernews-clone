c1afe76d15e9815170dc8451b5a6baf86894f99c
therealpanda98 <therealpanda@mail.ru>
2019-10-08 11:35:27 +0300

    Completed app

--- src/App.js
+++ src/App.js
@@ -1,82 +1,143 @@
 import React, { Component } from 'react';
+import { sortBy } from 'lodash';
 import './App.css';
-
-// const DEFAULT_QUERY = 'redux';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = '&page='
+const PARAM_PAGE = 'page=';
 
+const SORTS = {
+	NONE: list => list,
+	TITLE: list => sortBy(list, 'title'),
+	AUTHOR: list => sortBy(list, 'author'),
+	COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+	POINTS: list => sortBy(list, 'points').reverse(),
+}
+
+// Убираем записи с пустыми заголовками
 const isNotNullTitle = item => item.title !== null && item.title !== "";
 
+// Компонент поиска
 const Search = ({ value, onChange, onSubmit, children }) =>
-  <form onSubmit={onSubmit}>
-   <i className="fa fa-search"></i>
-	<input
-      type="text"
-      value={value}
-      onChange={onChange}
-   />
-	<button type="submit">
-		Поиск
-	</button>
-  </form>
-
-const Table = ({ list, onDismiss }) =>
-	<div className="table">
-		{list.filter(isNotNullTitle).map(item =>
-			<div key={item.objectID} className="table-row">
-				<span style={{ width: '40%' }}>
-					<a href={item.url}>{item.title}</a>
-				</span>
-				<span style={{ width: '20%' }}>
-					{item.author}
-				</span>
-				<span style={{ width: '10%' }}>
-					{item.author}
-				</span>
-				<span style={{ width: '10%' }}>
-					{item.num_comments}
-				</span>
-				<span style={{ width: '10%' }}>
-					{item.points}
-				</span>
-				<span style={{ width: '10%' }}>
-					<Button
-						onClick={() => onDismiss(item.objectID)}
-						className="button-inline"
-					>
-						Удалить
-					</Button>
-				</span>
-			</div>
-		)}
-  	</div>
-
-const PageControls = ({ onClickPrev, onClickNext, page }) =>
-	<div className="pageControls">
-		<Button
-			onClick={() => onClickPrev()}
-			className="btn-page-cntrl"
-		>
-			Prev
-		</Button>
-			{/* Вынести в компонент и сделать активным по нажатию */}
-			<span className="num-page active-page">{ page + 1 }</span>
-			<span className="num-page">{ page + 2 }</span>
-			<span className="num-page">{ page + 3 }</span>
-			{/* показывать когда возможно с тек страницы + 10 (макс 50 стр) */}
-			<span className="num-page">...</span>
-			<span className="num-page" onClick={() => onClickPrev()}>{ page + 11 }</span>
-		<Button
-			onClick={() => onClickNext()}
-			className="btn-page-cntrl"
-		>
-			Next
-		</Button>
+	<div className="interactions">
+		<form onSubmit={onSubmit}>
+		<i className="fa fa-search"></i>
+		<input
+			type="text"
+			value={value}
+			onChange={onChange}
+		/>
+		<button type="submit">
+			Find
+		</button>
+	</form>
+	{children}
 	</div>
 
+// Сортируемый заголовок
+const Sort = ({ sortKey, onSort, activeSortKey, children }) => {
+	// массив названий классов
+	const sortClass = ['button-inline'];
+
+	if (sortKey === activeSortKey) {
+		sortClass.push('button-active');
+	}
+
+	return (
+		<Button
+			onClick={() => onSort(sortKey)}
+			className={sortClass.join(' ')}
+		>
+			{children}
+		</Button>
+	);
+}
+
+// Компонент отображающий записи в строчку
+const Table = ({ list, sortKey, isSortReverse, onSort, onDismiss }) => {
+	list = list.filter(isNotNullTitle);
+
+	const sortedList = SORTS[sortKey](list);
+	const reverseSortedList = isSortReverse
+		? sortedList.reverse()
+		: sortedList;
+
+	return (
+		<div className="table">
+			<div className="table-header">
+				<span style={{ width: '40%' }}>
+					<Sort
+						sortKey={'TITLE'}
+						onSort={onSort}
+						activeSortKey={sortKey}
+					>
+						Title
+					</Sort>
+				</span>
+				<span style={{ width: '30%' }}>
+					<Sort
+						sortKey={'AUTHOR'}
+						onSort={onSort}
+						activeSortKey={sortKey}
+					>
+						Author
+					</Sort>
+				</span>
+				<span style={{ width: '10%' }}>
+					<Sort
+						sortKey={'COMMENTS'}
+						onSort={onSort}
+						activeSortKey={sortKey}
+					>
+						Comments
+					</Sort>
+				</span>
+				<span style={{ width: '10%' }}>
+					<Sort
+						sortKey={'POINTS'}
+						onSort={onSort}
+						activeSortKey={sortKey}
+					>
+						Points
+					</Sort>
+				</span>
+				<span style={{ width: '10%' }}>
+					Archive
+				</span>
+			</div>
+			{reverseSortedList.map(item =>
+				<div key={item.objectID} className="table-row">
+					<span style={{ width: '40%' }}>
+						<a href={item.url}>{item.title}</a>
+					</span>
+					<span style={{ width: '20%' }}>
+						{item.author}
+					</span>
+					<span style={{ width: '10%' }}>
+						{item.author}
+					</span>
+					<span style={{ width: '10%' }}>
+						{item.num_comments}
+					</span>
+					<span style={{ width: '10%' }}>
+						{item.points}
+					</span>
+					<span style={{ width: '10%' }}>
+						<Button
+							onClick={() => onDismiss(item.objectID)}
+							className="button-inline"
+						>
+							Удалить
+						</Button>
+					</span>
+				</div>
+			)}
+		</div>
+	);
+}
+
+// Компонент кнопки
 const Button = ({	onClick,	className = '', children, }) =>
   	<button
    	onClick={onClick}
@@ -86,112 +147,217 @@ const Button = ({	onClick,	className = '', children, }) =>
    	{children}
   	</button>
 
+const Loading = () => 
+	<div>Loading...</div>
+
+// HOC
+const withLoading = (Component) => ({ isLoading, ...other }) =>
+	isLoading
+		? <Loading/>
+		: <Component { ...other }/>
+	
+const ButtonWithLoading = withLoading(Button);
+
+// Main компонент
 class App extends Component {
+	// Состояние компонента на момент мотирования и размонтирования
+	_isMounted = false;
+
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			result: null,
+			results: null,
+			searchKey: '',
 			searchTerm: '',
-			page: 0,
+			error: null,
+			isLoading: false,
+			sortKey: 'NONE',
+			isSortReverse: false,
 		};
 
+		// Биндинг контекста
+		this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
 		this.setSearchTopStories = this.setSearchTopStories.bind(this);
 		this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
 		this.onSearchChange = this.onSearchChange.bind(this);
 		this.onSearchSubmit = this.onSearchSubmit.bind(this);
 		this.onDismiss = this.onDismiss.bind(this);
-		this.onClickPrev = this.onClickPrev.bind(this);
-		this.onClickNext = this.onClickNext.bind(this);
-	}
-	  
-	setSearchTopStories(result) {
-		this.setState({ result });
+		this.onSort = this.onSort.bind(this);
 	}
 
-	fetchSearchTopStories(searchTerm, page) {
-		console.log(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}${PARAM_PAGE}${page}`);
-		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}${PARAM_PAGE}${page}`)
-		.then(response => response.json())
-		.then(result => this.setSearchTopStories(result))
-		.catch(error => error);
+	// (Cache):: проверяет и возвращает !true/!false в зависимости от нахождения
+	//  поискового слова и его данных из API
+	needsToSearchTopStories(searchTerm) {
+		return !this.state.results[searchTerm];
+	}
+	
+	// Сохраняет и объединяет старые поиски с новыми в состояние results
+	setSearchTopStories(result) {
+		const { hits, page } = result;
+		const { searchKey, results } = this.state;
+
+		// Старые записи (если есть) для объединения с новыми (result)
+		const oldHits = results && results[searchKey]
+			? results[searchKey].hits
+			: [];
+
+		// Объединяем старые записи с новыми полученными из API
+		const updatedHits = [
+			...oldHits,
+			...hits
+		];
+
+		// results + новый запрос
+		// results имеет вид: {"@NameSearchTerm": 
+		// 								hits:{}, 
+		// 								page: 0} 
+		this.setState({ 
+			results: { 
+				...results,
+				[searchKey]: { hits: updatedHits, page: page }
+			},
+			isLoading: false
+		});
+	}
+
+	// Получение данных из API
+	fetchSearchTopStories(searchTerm, page = 0) {
+		this.setState({ isLoading: true });
+
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+			.then(response => response.json())
+			// Устанавливаем занчение/выводим ошибку, если компонент монитрован
+			// this._isMounted && this.setSearchTopStories(result) выполнится когда
+			// this._isMounted == true
+			.then(result => this._isMounted && this.setSearchTopStories(result))
+			.catch(error => this._isMounted && this.setState({ error: error }));
 	}
 		
+	// При вводе в поле input в форме search записывает занчение вводимого слова
   	onSearchChange(event) {
   		this.setState({ searchTerm: event.target.value });
 	}
 	
+	// При нажатии на кнопку сохраняем написанное слово в searchKey
 	onSearchSubmit(event) {
 		const { searchTerm } = this.state;
-		this.setState({ page: 0 });
-		const { page } = this.state;
-		this.fetchSearchTopStories(searchTerm, page);
+		this.setState({ searchKey: searchTerm });
+
+		// (Cache):: если такого поиского слова еще не вводили то делаем запрос API
+		// Иначе берем из кеша
+		if (this.needsToSearchTopStories(searchTerm)) {
+			this.fetchSearchTopStories(searchTerm);	
+		}
+
+		// При подтверждении формы отменяем обновление страницы
 		event.preventDefault();
 	}
-		
+	
+	// Удаление строки записи из результата, и сохраняем новый список записей в кеш
   	onDismiss(id) {
+		const { searchKey, results } = this.state;
+		const { hits, page } = results[searchKey];
+
 		const isNotId = item => item.objectID !== id;
-		const updatedHits = this.state.result.hits.filter(isNotId);
-		this.setState({ 
-			result: { ...this.state.result, hits: updatedHits }
+		const updatedHits = hits.filter(isNotId);
+
+		this.setState({
+			results: {
+				...results,
+				[searchKey]: {hits: updatedHits, page: page}
+			}
 		});
 	}
-	  
+
+	// Метод сортировки по нажатию на заголовки таблицы
+	onSort(sortKey) {
+		const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse;
+		this.setState({ sortKey, isSortReverse });
+	}
+	
+	// После первого render() метода компонента делаем запрос (1 запрос API)
 	componentDidMount() {
+		// Компонент монтирован
+		this._isMounted = true;
+
 		const { searchTerm, page } = this.state;
+		this.setState({ searchKey: searchTerm });
 		this.fetchSearchTopStories(searchTerm, page);
 	}
-
-	onClickPrev() {
-		const { searchTerm } = this.state;
-
-		if (this.state.page === 0) return 
-
-		this.setState(
-			{ page: this.state.page - 1 }, 
-			() => this.fetchSearchTopStories(searchTerm, this.state.page)
-		);
-	}
-
-	onClickNext() {
-		const { searchTerm } = this.state;
-
-		this.setState(
-			{ page: this.state.page + 1 }, 
-			() => this.fetchSearchTopStories(searchTerm, this.state.page)
-		);
+	
+	componentWillUnmount() {
+		// Компонент размонтирован
+		this._isMounted = false;
 	}
 
   	render() {
-		const { searchTerm, result } = this.state;
+		const {
+			searchTerm,
+			results,
+			searchKey,
+			error,
+			isLoading,
+			sortKey,
+			isSortReverse
+		} = this.state;
 
-		if (!result) { return null; }
+		// Если уже есть results И results со значением searchKey И у этого значения есть страница,
+		// то page = results[searchKey].page, иначе page = 0
+		const page = (
+			results && 
+			results[searchKey] &&
+			results[searchKey].page
+		) || 0;
+
+		// Если уже есть results И results со значением searchKey И у этого значения есть записи,
+		// то list = results[searchKey].hits, иначе page = 0
+		const list =(
+			results &&
+			results[searchKey] &&
+			results[searchKey].hits
+		) || [];
 
 		return (
 			<div className="page">
-				<div className="interactions">
-					{ console.log(this.state) }
-					<Search
-						value={searchTerm}
-						onChange={this.onSearchChange}
-						onSubmit={this.onSearchSubmit}
-					/>
-				</div>
-				{ result
-				  	? <Table
-					  list={result.hits}
-					  onDismiss={this.onDismiss}
-				  	/>
-					: null
+				{/* { console.log(this.state) } */}
+				<Search
+					value={searchTerm}
+					onChange={this.onSearchChange}
+					onSubmit={this.onSearchSubmit}
+				>
+				</Search>
+				{ error
+					? <div className="interactions">
+						<p>Something went wrong.</p>
+					</div>
+					: <div>
+						<Table
+							list={list}
+							sortKey={sortKey}
+							isSortReverse={isSortReverse}
+							onSort={this.onSort}
+							onDismiss={this.onDismiss}
+						/>
+						<div className="interactions">
+							<ButtonWithLoading
+								isLoading={isLoading}
+								onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+							>
+								Get more
+							</ButtonWithLoading>
+						</div>
+					</div>
 				}
-				<PageControls
-					onClickPrev={this.onClickPrev}
-					onClickNext={this.onClickNext}
-					page={this.state.page}
-				/>
       	</div>
    	);
   	}
 }
 
 export default App;
+
+export {
+	Button,
+	Search,
+	Table,
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App, { Search, Button, Table } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+
+	it('отрисовывает без ошибок', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	test('есть корректный снимок', () => {
+		const component = renderer.create(
+			<App />
+		);
+		const tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+});
+
+describe('Search', () => {
+
+	it('отрисовывает без ошибок', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Search>Поиск</Search>, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	test('есть корректный снимок', () => {
+		const component = renderer.create(
+			<Search>Поиск</Search>
+		);
+		const tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+});
+
+describe('Button', () => {
+
+	it('отрисовывает без ошибок', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Button>Дай мне больше</Button>, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	test('есть корректный снимок', () => {
+		const component = renderer.create(
+			<Button>Дай мне больше</Button>
+		);
+		const tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it('shows to button', () => {
+		const element = shallow(
+			<Button>Поиск</Button>
+		);
+
+		expect(element.length).toBe(1);
+	});
+
+});
+
+describe('Table', () => {
+
+	const props = {
+		list: [
+			{ title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+			{ title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+		],
+		sortKey: 'TITLE',
+		isSortReverse: false,
+	};
+
+	it('отрисовывает без ошибок', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Table { ...props } />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	test('есть корректный снимок', () => {
+		const component = renderer.create(
+			<Table { ...props } />
+		);
+		const tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it('shows to items in list', () => {
+		const element = shallow(
+			<Table { ...props } />
+		);
+
+		expect(element.find('.table-row').length).toBe(2);
+	})
+
 });
